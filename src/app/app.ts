09d985f5baa14cfece7ad0c21286c0d548f6dd50
Fabrizio <fsperduto9@gmail.com>
2025-08-29 14:21:05 -0300

    Primeiro commit

---
+++ src/app/app.ts
@@ -0,0 +1,21 @@
+// src/app/app.component.ts
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TaskListComponent } from './components/task-list/task-list'; // 1. IMPORTE
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    TaskListComponent // 2. ADICIONE AOS IMPORTS 
+],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'lista-tarefas-web';
+  toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+  }
+} 
+
---
+++ src/app/models/tarefa.ts
@@ -0,0 +1,6 @@
+// src/app/models/tarefa.ts
+export interface Tarefa {
+  id?: number; // O ID é opcional, pois o backend o gera na criação.
+  descricao: string;
+  concluida: boolean;
+}
